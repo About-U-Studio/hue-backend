fbd800aaf79aa0097285a479c95893971c4bf65d
About-U-Studio <Hello@aboutu.studio>
2025-11-04 02:44:52 -0500

    Update register.js

--- pages/api/register.js
+++ pages/api/register.js
@@ -6,10 +6,10 @@ import { isValidEmail, isValidName, validateRequiredFields, sanitizeString } fro
 import { rateLimitMiddleware } from '../../lib/rateLimit';
 import { generateAuthToken, getAuthTokenExpiration } from '../../lib/auth';
 import { generateVerificationToken, getTokenExpiration } from '../../lib/tokens';
-import { applySecurityHeaders } from '../../lib/securityHeaders.js';
-import { checkRequestSize } from '../../lib/requestLimits.js';
+import { applySecurityHeaders } from '../../lib/securityHeaders';
+import { checkRequestSize } from '../../lib/requestLimits';
 import { verifyCaptcha } from '../../lib/captcha';
-import { validatePassword, hashPassword, verifyPassword } from '../../lib/password.js';
+import { validatePassword, hashPassword, verifyPassword } from '../../lib/password';
 
 export default async function handler(req, res) {
   // Apply security headers
